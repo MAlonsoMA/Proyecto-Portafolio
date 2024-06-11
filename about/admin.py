a7f4e7a1583ed364f49261888ba73bd89fdf82ff
MAlonsoMA <MAlonsoMA@users.noreply.github.com>
2024-06-11 19:18:38 +0200

    Cambio en el admin.py de about. Espero que sea el último

--- about/admin.py
+++ about/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Course, Skill
 
-class AboutAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
-    list_display = ('id','title','created', 'updated')
+    list_display = ('title','degree_title','created', 'updated')
 
-admin.site.register([Course, Skill], AboutAdmin)
+admin.site.register(Course, CategoryAdmin)
+
+class SkillAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('title','image','created', 'updated')
+
+admin.site.register(Skill, SkillAdmin)
